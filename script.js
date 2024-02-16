3ed6e4b64c41ec58ea5ec201fd300f8b7d8d35af
Tirtho <tirthomondal.2001@gmail.com>
2024-02-17 00:39:30 +0600

    some changes to this

--- script.js
+++ script.js
@@ -41,3 +41,38 @@ let changeText = () => {
 
 changeText();
 setInterval(changeText, 3000);
+
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+} 
+
+
+// var icon= documnet.getElementById("icon");
+// icon.onclick= function()
+// {
+//     document.body.classList.toggle("dark-theme");
+//     if(document.body.classList.contains("dark-theme")){
+//         icon.src="img/sun.png";
+//     }else{
+//         icon.src="img/moon.png";
+//     }
+// }
+var icon = document.getElementById("icon");
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        document.getElementById("icon").src = "img/sun.png";
+    } else {
+        document.getElementById("icon").src = "img/moon.png";
+    }
+}
+
+
+// mix it up portfolio section
+document.addEventListener('DOMContentLoaded', function() {
+    var mixer = mixitup('.portfolio-gallery');
+});
